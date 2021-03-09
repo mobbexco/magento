6dbe0eea03bc4ccf831028528cf0a462dbaf0e2f
jlmobbex <javier.lopez@mobbex.com>
2021-03-09 17:34:31 -0300

    Remove duplicate install script

--- app/code/local/Mobbex/Mobbex/sql/mobbex_mobbex_setup/install-1.3.0.php
+++ app/code/local/Mobbex/Mobbex/sql/mobbex_mobbex_setup/install-1.3.0.php
@@ -4,19 +4,19 @@ $installer = $this;
 $installer->startSetup();
 
 $tableTransaction = $installer->getConnection()
-        ->newTable($installer->getTable('mobbex/transaction'))
-        ->addColumn('transaction_mobbex_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-            'identity'  => true,
-            'unsigned'  => true,
-            'nullable'  => false,
-            'primary'   => true,
-            ), 'Id')
-        ->addColumn('order_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-            'nullable'  => false,
-            ), 'Order id')
-        ->addColumn('data', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
-            'nullable'  => false,
-            ), 'Data');
+    ->newTable($installer->getTable('mobbex/transaction'))
+    ->addColumn('transaction_mobbex_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'unsigned'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+        ), 'Id')
+    ->addColumn('order_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'nullable'  => false,
+        ), 'Order id')
+    ->addColumn('data', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+        'nullable'  => false,
+        ), 'Data');
 
 $installer->getConnection()->createTable($tableTransaction);            
 $installer->endSetup();
