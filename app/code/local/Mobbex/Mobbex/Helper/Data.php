5abd284868898ed30d64c442f275bf800ebf474f
jlmobbex <javier.lopez@mobbex.com>
2021-07-09 23:01:12 -0300

    Add get sources request methods to helper

--- app/code/local/Mobbex/Mobbex/Helper/Data.php
+++ app/code/local/Mobbex/Mobbex/Helper/Data.php
@@ -260,4 +260,88 @@ class Mobbex_Mobbex_Helper_Data extends Mage_Core_Helper_Abstract
         }
         return $cuit; 
     }
+
+	/**
+	 * Get sources with common plans from mobbex.
+	 * 
+	 * @param null|int $total
+	 * 
+	 * @return array
+	 */
+	public function getSources($total = null)
+	{
+		$curl = curl_init();
+
+		$data = $total ? '?total=' . $total : null;
+
+		curl_setopt_array($curl, [
+			CURLOPT_URL 		   => "https://api.mobbex.com/p/sources$data",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING 	   => "",
+			CURLOPT_MAXREDIRS 	   => 10,
+			CURLOPT_TIMEOUT 	   => 30,
+			CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST  => "GET",
+			CURLOPT_HTTPHEADER	   => $this->getHeaders()
+		]);
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+			Mage::log('Curl Error #:' . $err);
+			Mage::throwException('Curl Error #:' . $err);
+		} else {
+			$res = json_decode($response, true);
+
+			if ($res['data']) {
+				return $res['data'];
+			}
+		}
+
+		return [];
+	}
+
+	/**
+	 * Get sources with advanced rule plans from mobbex.
+	 * 
+	 * @param string $rule
+	 * 
+	 * @return array
+	 */
+	public function getSourcesAdvanced($rule = 'externalMatch')
+	{
+		$curl = curl_init();
+
+		curl_setopt_array($curl, [
+			CURLOPT_URL 		   => str_replace('{rule}', $rule, 'https://api.mobbex.com/p/sources/rules/{rule}/installments'),
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING 	   => "",
+			CURLOPT_MAXREDIRS 	   => 10,
+			CURLOPT_TIMEOUT 	   => 30,
+			CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST  => "GET",
+			CURLOPT_HTTPHEADER	   => $this->getHeaders()
+		]);
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+			Mage::log('Curl Error #:' . $err);
+			Mage::throwException('Curl Error #:' . $err);
+		} else {
+			$res = json_decode($response, true);
+
+			if ($res['data']) {
+				return $res['data'];
+			}
+		}
+
+		return [];
+	}
 }
