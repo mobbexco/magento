0e42a0993ad45f3a131318ef6c3598bd8658f175
jlmobbex <javier.lopez@mobbex.com>
2021-07-09 23:35:41 -0300

    Tabs save new plans. Category use block to render

--- app/code/local/Mobbex/Mobbex/Model/Observer.php
+++ app/code/local/Mobbex/Mobbex/Model/Observer.php
@@ -2,152 +2,67 @@
 
 class Mobbex_Mobbex_Model_Observer
 {
-    /**
-     * Flag to stop observer executing more than once
-     *
-     */
+	/** @var Mobbex_Mobbex_Helper_Settings */
+	public $settings;
+
+    /** Flag to stop observer executing more than once */
     static protected $_singletonFlag = false;
 
-    public function saveProductTabData()
-    {
-        if (!self::$_singletonFlag) {
-            self::$_singletonFlag = true;
-
-            $product = Mage::registry('current_product');
-
-            // Check for catch associated products save
-            if (!empty($product)) {
-                $product_id = $product->getId();
-
-                try {
-                    $plans = array(
-                        'ahora_3' => Mage::app()->getRequest()->getPost('ahora_3'),
-                        'ahora_6' => Mage::app()->getRequest()->getPost('ahora_6'),
-                        'ahora_12' => Mage::app()->getRequest()->getPost('ahora_12'),
-                        'ahora_18' => Mage::app()->getRequest()->getPost('ahora_18'),
-                    );
-
-                    foreach ($plans as $key => $value) {
-                        if ($value === 'on') {
-                            Mage::getModel('mobbex/customfield')->saveCustomField($product_id, 'product', $key, 'yes');
-                        } else {
-                            Mage::getModel('mobbex/customfield')->saveCustomField($product_id, 'product', $key, 'no');
-                        }
-                    }
-
-                    return true;
-                } catch (Exception $e) {
-                    Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-                }
-            }
-        }
-    }
-
-	public function saveCategoryTabData()
-	{
-		if (!self::$_singletonFlag) {
-			self::$_singletonFlag = true;
-			try {
-				$category_id = Mage::registry('current_category')->getId();
-				if(!empty($category_id)){
-				
-					$plans = array(
-						'ahora_3' => Mage::app()->getRequest()->getPost('ahora_3'),
-						'ahora_6' => Mage::app()->getRequest()->getPost('ahora_6'),
-						'ahora_12' => Mage::app()->getRequest()->getPost('ahora_12'),
-						'ahora_18' => Mage::app()->getRequest()->getPost('ahora_18'),
-					);
-
-					foreach ($plans as $key => $value) {
-						if ($value === 'on') {
-							Mage::getModel('mobbex/customfield')->saveCustomField($category_id, 'category', $key, 'yes');
-						} else {
-							Mage::getModel('mobbex/customfield')->saveCustomField($category_id, 'category', $key, 'no');
-						}
-					}
-
-					return true;
-				}
-			} catch (Exception $e) {
-				Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-			}
-		}
-		return false;
+	public function __construct() {
+		$this->settings	= Mage::helper('mobbex/settings');
 	}
 
-
 	/**
-	 * Add a new tab in the backoffice category page 
+	 * Add a new tab in the backoffice category page.
+	 * 
+	 * @param Varien_Event_Observer $observer
 	 */
 	public function newTabCategory($observer)
 	{
-        if (!self::$_singletonFlag) {
-            self::$_singletonFlag = true;
-            $tabs = $observer->getTabs();
-            $category_id = Mage::registry('current_category')->getId();
-			if(!empty($category_id)){
-				// Get plans with current values
-				$plans = $this->getPlans($category_id);
-				$plans_html = '';
-				$full_html = '';
+		if (self::$_singletonFlag)
+			return;
 
-				foreach ($plans as $plan => $data) {
-					$enabled = ($data['value'] === 'yes');
-					$plans_html .=
-								"<tr>
-									<td class='value'>
-										<input type='checkbox' name='".$plan."' id='".$plan."' " . ($enabled ? 'checked' : '') . ">
-									</td>
-									<td class='label' style='width: 100%;'><label for='" . $plan . "'>" . $data['label'] . "</label></td>
-								</tr>";
-				}
+		self::$_singletonFlag = true;
 
-				$full_html ='
-					<div class="entry-edit">
-						<div class="entry-edit-head">
-							<h4 class="icon-head head-edit-form fieldset-legend">Mobbex Options</h4>
-						</div>
-						<div class="fieldset fieldset-wide" id="mobbex_options">
-							<div class="hor-scroll">
-								<table cellspacing="0" class="form-list">
-									<tbody>
-										<h3>Elija los planes que NO quiera que aparezcan durante la compra</h3>
-										'.$plans_html.'
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>';
-				$tabs->addTab('customtab', array(
-					'label'     => 'Mobbex Plans',
-					'content'   => $full_html
-				));
-			}
-		}
+		$tabs = $observer->getTabs();
+
+		// Create Mobbex Options tab
+		$tabs->addTab('mobbex_configuration', [
+			'label'   => 'Mobbex Options',
+			'content' => $tabs->getLayout()->createBlock('mobbex/adminhtml_catalog_category_tab')->toHtml()
+		]);
     }
 
-	private  function getPlans($category_id)
-    {
-		$ahora = array(
-			'ahora_3' => array(
-				'label' => 'Ahora 3',
-				'value' => Mage::getModel('mobbex/customfield')->getCustomField($category_id, 'category', 'ahora_3'),
-			),
-			'ahora_6' => array(
-				'label' => 'Ahora 6',
-				'value' => Mage::getModel('mobbex/customfield')->getCustomField($category_id, 'category', 'ahora_6'),
-			),
-			'ahora_12' => array(
-				'label' => 'Ahora 12',
-				'value' => Mage::getModel('mobbex/customfield')->getCustomField($category_id, 'category', 'ahora_12'),
-			),
-			'ahora_18' => array(
-				'label' => 'Ahora 18',
-				'value' => Mage::getModel('mobbex/customfield')->getCustomField($category_id, 'category', 'ahora_18'),
-			),
-		);
-		return $ahora;
-    }
+	public function saveProductTabData()
+	{
+		if (self::$_singletonFlag)
+			return;
+
+		self::$_singletonFlag = true;
+
+		$id = Mage::registry('current_product') ? Mage::registry('current_product')->getId() : false;
+
+		// Exit if it's associated products save
+		if (empty($id))
+			return;
+
+		$this->settings->savePlanFields($id);
+	}
+
+	public function saveCategoryTabData()
+	{
+		if (self::$_singletonFlag)
+		return;
+
+		self::$_singletonFlag = true;
+
+		$id = Mage::registry('current_category') ? Mage::registry('current_category')->getId() : false;
+
+		if (empty($id))
+			return;
+
+		$this->settings->savePlanFields($id, 'category');
+	}
 
 	/**
 	 * Calculates the refund amount of an order
