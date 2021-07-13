93110da566ea1db535ee4fe2b7f33bae790b740f
jlmobbex <javier.lopez@mobbex.com>
2021-07-12 21:12:08 -0300

    Delete local env files and update gitignore

--- build.sh
+++ build.sh
@@ -7,6 +7,12 @@ MAGE_V="1.6-1.9"
 
 mkdir mobbex
 cp -R app mobbex/app
-cp Mobbex_Mobbex.xml mobbex/
-zip mobbex.$VER.mage-$MAGE_V.zip -r mobbex
+cp -R skin mobbex/skin
+
+if type 7z > /dev/null; then
+    7z a -tzip "mobbex.$VER.mage-$MAGE_V.zip" mobbex
+elif type zip > /dev/null; then
+    zip mobbex.$VER.mage-$MAGE_V.zip -r mobbex
+fi
+
 rm -Rf mobbex
