5682e373affa3a4a0d2205d8b9f7cd5a895f5105
EMunozMobbex <e.munoz.chedel@mobbex.com>
2022-12-26 12:24:18 -0300

    feat: Added log callback to sdk platform.

--- app/code/local/Mobbex/Mobbex/Helper/Sdk.php
+++ app/code/local/Mobbex/Mobbex/Helper/Sdk.php
@@ -6,7 +6,7 @@ class Mobbex_Mobbex_Helper_Sdk extends Mage_Core_Helper_Abstract
     public $instantiator;
 
     public function __construct() {
-        \Mage::helper('mobbex/instantiator')->setProperties($this, ['settings', 'helper']);
+        \Mage::helper('mobbex/instantiator')->setProperties($this, ['settings', 'helper', 'logger']);
     }
 
     /**
@@ -16,11 +16,15 @@ class Mobbex_Mobbex_Helper_Sdk extends Mage_Core_Helper_Abstract
     {
         // Set platform information
         \Mobbex\Platform::init('magento_1', '2.0', Mage::getBaseUrl(),
-        [
-            'magento' => Mage::getVersion(),
-            'webpay'  => 2.0,
-            'sdk'     => \Composer\InstalledVersions::getVersion('mobbexco/php-plugins-sdk'),
-        ], $this->settings->getAll(), [$this->helper, 'executeHook']);
+            [
+                'magento' => Mage::getVersion(),
+                'webpay'  => 2.0,
+                'sdk'     => \Composer\InstalledVersions::getVersion('mobbexco/php-plugins-sdk'),
+            ], 
+            $this->settings->getAll(), 
+            [$this->helper, 'executeHook'],
+            [$this->logger, 'log']
+        );
 
         // Init api conector
         \Mobbex\Api::init();
