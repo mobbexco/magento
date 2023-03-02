1d86d74c930ddc8e5680fce58b37f70acf227f2e
jlmobbex <javier.lopez@mobbex.com>
2023-03-02 11:19:45 -0300

    Fix template obtaining on payment info block

--- app/code/local/Mobbex/Mobbex/Block/Adminhtml/Payment/Info.php
+++ app/code/local/Mobbex/Mobbex/Block/Adminhtml/Payment/Info.php
@@ -1,8 +1,11 @@
-<?php class Mobbex_Mobbex_Block_Adminhtml_Payment_Info extends Mage_Payment_Block_Info
+<?php
+
+class Mobbex_Mobbex_Block_Adminhtml_Payment_Info extends Mage_Payment_Block_Info
 {
     protected function _construct()
     {
         parent::_construct();
+        $this->setTemplate('mobbex/info.phtml');
 
         $this->mobbexTransaction = Mage::getModel('mobbex/transaction');
     }
