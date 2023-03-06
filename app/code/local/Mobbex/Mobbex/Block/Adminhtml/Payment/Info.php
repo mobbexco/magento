7605eb097d08a18e675dbab70ad0f567fbfa92f7
mobbexLuis <luis.scarrone@mobbex.com>
2023-03-06 16:41:21 -0300

    filter cards method deleted

--- app/code/local/Mobbex/Mobbex/Block/Adminhtml/Payment/Info.php
+++ app/code/local/Mobbex/Mobbex/Block/Adminhtml/Payment/Info.php
@@ -9,28 +9,4 @@ class Mobbex_Mobbex_Block_Adminhtml_Payment_Info extends Mage_Payment_Block_Info
 
         $this->mobbexTransaction = Mage::getModel('mobbex/transaction');
     }
-
-    /**
-     * Eliminates duplicated cards.
-     * @param array
-     * @return array
-     */
-    public function filterCards($cards)
-    {
-        if(!$cards)
-            return false;
-        $filter = [];
-        $data = [];
-        foreach ($cards as $key => $card) {
-            $filter['_' . $key] = $card['payment_id'];
-        }
-        $filter = array_unique($filter);
-        foreach ($cards as $key => $card) {
-            $pos = '_' . $key;
-            if (isset($filter[$pos])) {
-                $data[] = $card;
-            }
-        }
-        return $data;
-    }
 }
