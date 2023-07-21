a807aa1543eff92d13bea647132a25dbc37bffaa
jlmobbex <javier.lopez@mobbex.com>
2023-07-21 19:03:19 -0300

    Fix id obtaining on product and category settings

--- app/code/local/Mobbex/Mobbex/Block/Adminhtml/Catalog/Category/Tab.php
+++ app/code/local/Mobbex/Mobbex/Block/Adminhtml/Catalog/Category/Tab.php
@@ -4,13 +4,12 @@ class Mobbex_Mobbex_Block_Adminhtml_Catalog_Category_Tab extends Mage_Adminhtml_
 {
 	public function _construct()
 	{
-		$id = Mage::registry('current_category') ? Mage::registry('current_category')->getId() : false;
+		$this->rowId = Mage::registry('current_category') ? Mage::registry('current_category')->getId() : false;
 
-		if (empty($id))
+		if (!$this->rowId)
 			return;
 
 		\Mage::helper('mobbex/instantiator')->setProperties($this, ['sdk', 'settings', 'helper']);
-		$this->idCategory = $id;
 		
 		$this->setTemplate('mobbex/category-settings.phtml');
 	}
